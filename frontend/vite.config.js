d7be611179622a6a2cdf730d68af284d6e2b6042
Nainwalshivam <154865002+Nainwalshivam@users.noreply.github.com>
2024-08-20 16:15:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://chat-application-backend-y5lg.onrender.com",
         secure: false,
       },
     },
