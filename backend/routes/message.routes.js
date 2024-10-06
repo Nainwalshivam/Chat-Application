03416d829a9ff25f2446bdf46a27486beffe7121
Nainwalshivam <154865002+Nainwalshivam@users.noreply.github.com>
2024-10-06 13:16:55 +0530

    Update(Pratice) message.routes.js

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -4,7 +4,10 @@ import { getMessage, sendMessage } from "../controller/message.controller.js"
 
 const router = express.Router()
 
-router.get("/:id", isAuthenticate, getMessage)
+router.get("/:id",(req, res) => {
+    console.log(req.body);  // Log incoming request data
+    // Process the request
+})
 router.post("/send/:id", isAuthenticate, sendMessage)
 
 export default router
