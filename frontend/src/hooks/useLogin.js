63f902180ef56e43065971c43ba675df57e1351c
Nainwalshivam <154865002+Nainwalshivam@users.noreply.github.com>
2024-10-07 14:44:55 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -27,7 +27,7 @@ const useLogin = () => {
     try {
       setLoading(true)
 
-      const res = await fetch("https://chat-application-backend-y5lg.onrender.com/api/auth/login", {
+      const res = await fetch("api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
